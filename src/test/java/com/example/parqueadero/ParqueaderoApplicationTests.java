c8e18b0665497328690ea26922af10448bf2bf03
Santiago López García - Ceiba Software <santiago.lopez@MacBook-Air-de-Usuario.local>
2018-02-01 08:05:13 -0500

    iniciar pruebas unitarias

--- src/test/java/com/example/parqueadero/ParqueaderoApplicationTests.java
+++ src/test/java/com/example/parqueadero/ParqueaderoApplicationTests.java
@@ -1,16 +1,32 @@
 package com.example.parqueadero;
 
+import java.awt.List;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.example.parqueadero.model.TipoVehiculo;
+import com.example.parqueadero.repository.TipoVehiculoRepositorio;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ParqueaderoApplicationTests {
 
+	private TipoVehiculoController tipoVehiculoController;
+	private TipoVehiculoRepositorio tipoVehiculoRepositorio;
+	
+	@Before
+	public void setUp(){
+		tipoVehiculoController = new TipoVehiculoController();
+	}
+	
 	@Test
-	public void contextLoads() {
+	public void metodoGetTiposVehiculosRetornaUnaLista() {
+		Assert.assertEquals(tipoVehiculoController.getTodosTiposVehiculo(), new List());
 	}
 
 }
